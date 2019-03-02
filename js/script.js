9a96fecf7f7efafc7e13e2b9e261cfaa68d3a867
Malgorzata Lesniak <M999@interia.eu>
2019-03-02 22:35:40 +0100

    Change scroll

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ var flkty = new Flickity( carousel, {
   percentPosition: false,
   wrapAround:true,
   pageDots:false,
-  autoPlay: true
+  autoPlay: true,
+  hash: true
 });
 
 var imgs = carousel.querySelectorAll('.carousel-cell img');
@@ -18,14 +19,9 @@ var transformProp = typeof docStyle.transform == 'string' ?
 
 var progressBar = document.querySelector('.progress-bar');
 
-flkty.on( 'scroll', function(progress) {
-  flkty.slides.forEach( function( slide, i ) {
-    var img = imgs[i];
-    var x = ( slide.target + flkty.x ) * -1/3;
-    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
-    progress = Math.max( 0, Math.min( 1, progress ) );
-    progressBar.style.width = progress * 100 + '%';
-  });
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
 
 var restart = document.querySelector('.button');
