c90db5cadc90a327aebb0a359037ccf35e7a7140
Malgorzata Lesniak <M999@interia.eu>
2019-03-13 18:50:01 +0100

    Add description to img

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@
 // external js: flickity.pkgd.js
 
 (function() { 
-  //stworzenie zmiennych z kodem szablonów
+  //stworzenie zmiennej z kodem szablonów
   var templateList = document.getElementById('template-lista-slajdow').innerHTML;
-  var templateImage = document.getElementById('template-opis-slajdu').innerHTML; 
  //po znalezieniu elementu z wstawiana wartoscia pobieramy jego zawartość za pomocą właściwości innerHTML
  // Następnie opcjonalnie możemy wykonać poniższą linię kodu, która sprawi że nasz szablon będzie działał szybciej.  
 
- Mustache.parse(templateImage);
+ Mustache.parse(templateList);
 
 var listaSlajdow = '';	// Tworzymy zmienną, w której chcemy mieć kod HTML wszystkich slajdów. 
 /* Pętla dla każdego elementu z listy:
@@ -16,10 +15,7 @@ var listaSlajdow = '';	// Tworzymy zmienną, w której chcemy mieć kod HTML wsz
   2. dodaje ten wygenrowany kod HTML do zmiennej listaSlajdow. */
 
 for (var i = 0; i < dataImages.length; i++){
-  console.log(dataImages);
-  listaSlajdow+= Mustache.render(templateList, dataImages[i]); // Wygenerowanie slajdu z tłem,
-  templateList.id = toString(i);// dodanie id
-  listaSlajdow+= Mustache.render(templateImage, dataImages[i]); // Dodanie tytułu i opisu
+  listaSlajdow+= Mustache.render(templateList, dataImages[i]); // Wygenerowanie slajdu z tłem, dodanie tytułu i opisu
   }
 
  // Pozostaje nam dodać wygenerowany kod HTML na naszej stronie, na końcu diva z id="results".
@@ -38,6 +34,7 @@ var flkty = new Flickity( carousel, {
   hash: true
 });
 var imgs = carousel.querySelectorAll('.carousel-cell img');
+ 
 // get transform property
 var docStyle = document.documentElement.style;
 var transformProp = typeof docStyle.transform == 'string' ?
@@ -47,7 +44,7 @@ var progressBar = document.querySelector('.progress-bar');
 
 flkty.on( 'scroll', function(progress) {
   flkty.slides.forEach( function( slide, i ) {
-    var x = ( slide.target + flkty.x ) * -1/3;
+    var x = ( slide.target + flkty.x );
     imgs[i].style[ transformProp ] = 'translateX(' + x  + 'px)';
     progress = Math.max( 0, Math.min( 1, progress ) );
     progressBar.style.width = progress * 100 + '%';
@@ -61,7 +58,5 @@ restart.addEventListener( 'click', function( event ) {
 });
 
 
-function newFunction() {
-  document.write('Parse:');
-}
+
 
